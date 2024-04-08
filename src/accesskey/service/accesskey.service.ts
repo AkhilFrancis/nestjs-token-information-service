211f8cdf5b8a21f39b6d7ce60ddb9843e61c0274
Akhil Francis <akhilfrancis0007@gmail.com>
2024-04-08 21:52:53 +0530

    Used Config Service and Env variable to get access key microservice endpoint

--- src/accesskey/service/accesskey.service.ts
+++ src/accesskey/service/accesskey.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { Accesskey } from '../interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AccesskeyService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async getAccessKey(accessKey: string): Promise<Accesskey> {
     try {
       // Make request to Microservice 1 to validate access key
       const response = await firstValueFrom(
-        this.httpService.get(`http://localhost:3000/access-keys/${accessKey}`),
+        this.httpService.get(
+          `${this.configService.get('ACCESSKEY_MICROSERVICE_ENDPOINT')}/access-keys/${accessKey}`,
+        ),
       );
       return response.data;
     } catch (error) {
